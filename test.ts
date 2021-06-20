ec9750ce227035547287bac3e8b97041068abe80
ocpu <martin.hovre@opencubes.io>
2021-06-20 02:08:20 +0200

    fix(demo): Made labels clearer

--- test.ts
+++ test.ts
@@ -5,15 +5,15 @@ await question('list', 'List?', ['hello', 'world'])
 await question('checkbox', 'Checkboxes?', ['hello', 'world'])
 await question('input', 'Input?')
 await question('input', 'Input with default?', 'default')
-console.log('Result: %s', await question('password', 'Password? (will print)'))
-console.log('Result: %s', await question('password', 'Password? (will print)', '<>'))
-console.log('Result: %s', await question('password', 'Password? (will print)', false))
-console.log('Result: %s', await question('list', 'List with object? (will print)', {
+console.log('Result: %s', await question('password', 'Password? (will print result)'))
+console.log('Result: %s', await question('password', 'Password with custom pattern? (will print result)', '<>'))
+console.log('Result: %s', await question('password', 'Password with no printing characters? (will print result)', false))
+console.log('Result: %s', await question('list', 'List with object? (will print result)', {
   'Cheese': 'cheese',
   'Milk': 'milk',
   'Tofu': 'tofu',
 }))
-console.log('Result: %s', await question('checkbox', 'Select stuff with object? (will print)', {
+console.log('Result: %s', await question('checkbox', 'Select stuff with object? (will print result)', {
   'Cheese': { value: 'cheese', selected: true },
   'Garlic': { value: 'garlic' },
   'Salami': { value: 'salami' },
